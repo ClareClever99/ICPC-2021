bd9b7bdd2a7f8b8940ea2b1b7aee6ab7e2f3c73d
Clare Clever <59885439+Robert-Clever@users.noreply.github.com>
2021-02-24 09:32:35 -0500

    Added new code...

--- Practice_Problems/R2/Java/R2.java
+++ Practice_Problems/R2/Java/R2.java
@@ -6,10 +6,13 @@ public class R2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNextLine()) {
-            int r1 = sc.nextInt();
-            int s = sc.nextInt();
+        String line = sc.nextLine();
+        while (!(line.isEmpty())) {
+            String[] list = line.split(" ");
+            int r1 = Integer.parseInt(list[0]);
+            int s = Integer.parseInt(list[1]);
             System.out.println((2 * s) - r1);
+            line = sc.nextLine();
         }
         sc.close();
     }
