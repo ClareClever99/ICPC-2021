18e68e39ea3ad450e1e477365950f474c3c95fde
Clare Clever <59885439+Robert-Clever@users.noreply.github.com>
2021-02-23 21:05:46 -0500

    Added new code...

---
+++ Practice_Problems/Stuck_In_A_Time_Loop/Java/time.java
@@ -0,0 +1,18 @@
+package Practice_Problems.Stuck_In_A_Time_Loop.Java;
+
+import java.util.Scanner;
+
+public class time {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLong()) {
+            long input = sc.nextLong();
+            for (int i = 0; i < input; i++) {
+                System.out.println((i + 1) + " Abracadabra");
+            }
+        }
+        sc.close();
+    }
+
+}
