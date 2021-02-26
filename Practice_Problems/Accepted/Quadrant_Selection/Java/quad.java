7bf5da33668457cf922ea8bfd9f7766d8ad3cd04
Clare Clever <59885439+Robert-Clever@users.noreply.github.com>
2021-02-26 07:55:24 -0500

    Changed some code around...

--- Practice_Problems/Accepted/Quadrant_Selection/Java/quad.java
+++ Practice_Problems/Accepted/Quadrant_Selection/Java/quad.java
@@ -6,19 +6,16 @@ public class quad {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNextLong()) {
-            long x = sc.nextLong();
-            long y = sc.nextLong();
-            if (x > 0 && y > 0) {
-                System.out.println(1);
-            } else if (x < 0 && y > 0) {
-                System.out.println(2);
-            } else if (x < 0 && y < 0) {
-                System.out.println(3);
-            } else {
-                System.out.println(4);
-            }
-
+        long x = sc.nextLong();
+        long y = sc.nextLong();
+        if (x > 0 && y > 0) {
+            System.out.println(1);
+        } else if (x < 0 && y > 0) {
+            System.out.println(2);
+        } else if (x < 0 && y < 0) {
+            System.out.println(3);
+        } else {
+            System.out.println(4);
         }
         sc.close();
     }
