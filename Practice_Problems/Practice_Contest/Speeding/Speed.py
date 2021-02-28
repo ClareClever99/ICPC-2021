5a69ada659f603176f0520703c8cf8bdb8ef68a0
Clare Clever <59885439+Robert-Clever@users.noreply.github.com>
2021-02-28 14:58:39 -0500

    New code...

--- Practice_Problems/Practice_Contest/Speeding/Speed.py
+++ Practice_Problems/Practice_Contest/Speeding/Speed.py
@@ -1,21 +1,27 @@
 
 photoCount = int(input())
 photos = []
-currenSpeed = []
 
 for i in range(photoCount):
     photos.append(input())
 
-currPhoto = 0
+prevPhoto = 0
+currPhoto = 1
 highestSpeed = 0
 
 while(currPhoto < photoCount):
     time = int((photos[currPhoto].split(" "))[0])
     distance = int((photos[currPhoto].split(" "))[1])
-    if (time != 0 and distance != 0):
-        currenSpeed.append(distance/time)
-        if(currSpeed > highestSpeed):
-            highestSpeed = currSpeed
+
+    prevTime = int((photos[prevPhoto].split(" "))[0])
+    prevDistance = int((photos[prevPhoto].split(" "))[1])
+
+    currSpeed = ((distance - prevDistance) / (time - prevTime))
+
+    if(currSpeed > highestSpeed):
+        highestSpeed = currSpeed
+
     currPhoto += 1
+    prevPhoto += 1
 
 print(int(highestSpeed))
