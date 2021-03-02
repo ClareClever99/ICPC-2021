e2f256fbd461cf2b272e1763c3a5efe73fb06daf
Clare Clever <59885439+Robert-Clever@users.noreply.github.com>
2021-03-02 10:38:52 -0500

    Added new code...

---
+++ Practice_Problems/inProgress/Pot/Python/pot.py
@@ -0,0 +1,13 @@
+numLines = int(input())
+
+inputNumbers = []
+
+total = 0
+
+for i in range(numLines):
+    inputNumbers.append(input())
+
+for number in inputNumbers:
+    print()
+
+print(inputNumbers)
